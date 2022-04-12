c26f4be0b70ea7b4c834a100efb809b894ba7dde
Vineet Kumar <vk.vempire@gmail.com>
2022-04-12 18:23:51 +0530

    added swap

--- contract/contracts/dDex.sol
+++ contract/contracts/dDex.sol
@@ -102,4 +102,74 @@ contract dLandExchange is ERC20 {
         ERC20(dldTokenAddress).transfer(msg.sender, dldTokenAmount);
         return (ethAmount, dldTokenAmount);
     }
+
+    /** 
+    @dev Returns the amount Eth/DLD tokens that would be returned to the user
+    * in the swap
+    */
+     function getAmountOfTokens(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) public pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+        // We are charging a fees of `1%`
+        // Input amount with fees = (input amount - (1*(input amount)/100)) = ((input amount)*99)/100
+        uint256 inputAmountWithFee = inputAmount * 99;
+        // Because we need to follow the concept of `XY = K` curve
+        // We need to make sure (x + Δx)*(y - Δy) = (x)*(y)
+        // so the final formulae is Δy = (y*Δx)/(x + Δx);
+        // Δy in our case is `tokens to be recieved`
+        // Δx = ((input amount)*99)/100, x = inputReserve, y = outputReserve
+        // So by putting the values in the formulae you can get the numerator and denominator
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denominator = (inputReserve * 100) + inputAmountWithFee;
+        return numerator / denominator;
+    }
+
+    /** 
+    @dev Swaps Ether for DLD Tokens
+    */
+    function ethTodldToken(uint _minTokens) public payable {
+        uint256 tokenReserve = getReserve();
+        // call the `getAmountOfTokens` to get the amount of crypto dev tokens
+        // that would be returned to the user after the swap
+        // Notice that the `inputReserve` we are sending is equal to  
+        //  `address(this).balance - msg.value` instead of just `address(this).balance`
+        // because `address(this).balance` already contains the `msg.value` user has sent in the given call
+        // so we need to subtract it to get the actual input reserve
+        uint256 tokensBought = getAmountOfTokens(
+            msg.value,
+            address(this).balance - msg.value,
+            tokenReserve
+        );
+
+        require(tokensBought >= _minTokens, "insufficient output amount");
+        // Transfer the `DLD` tokens to the user
+        ERC20(dldTokenAddress).transfer(msg.sender, tokensBought);
+    }
+
+
+    /** 
+    @dev Swaps dld Tokens for Ether
+    */
+    function dldTokenToEth(uint _tokensSold, uint _minEth) public {
+       uint256 tokenReserve = getReserve();
+        // call the `getAmountOfTokens` to get the amount of ether
+        // that would be returned to the user after the swap
+        uint256 ethBought = getAmountOfTokens(
+            _tokensSold,
+            tokenReserve,
+            address(this).balance
+        );
+        require(ethBought >= _minEth, "insufficient output amount");
+        // Transfer `DLD` tokens from the user's address to the contract
+        ERC20(dldTokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _tokensSold
+        );
+        // send the `ethBought` to the user from the contract
+        payable(msg.sender).transfer(ethBought);
+    }
 }
